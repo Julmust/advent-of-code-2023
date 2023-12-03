cbacbe2d412d9edabb0cc33fdd7cd8eb53238286
Jesper Bergeskans <jesperbergeskans@devoteam.com>
2023-12-03 15:15:01 +0100

    Solution for part 2

--- day_3/main.go
+++ day_3/main.go
@@ -9,6 +9,7 @@ import (
 
 type coord struct {
 	x, y int
+	val  string
 }
 
 func display(data [][]string) {
@@ -102,7 +103,7 @@ func getNumberCoordinates(data [][]string) [][]coord {
 		for sidx, sval := range val {
 			_, err := strconv.Atoi(sval) // Trying to cast from string to int
 			if err == nil {              // If the cast succeeded, we know the value is an integer
-				tmp_co = append(tmp_co, coord{x: idx, y: sidx})
+				tmp_co = append(tmp_co, coord{x: idx, y: sidx, val: sval})
 			} else {
 				// If the cast failed, and we have values in our "cache"
 				// we flush those to the main 2d array and clear the cache.
@@ -129,6 +130,106 @@ func getNumberCoordinates(data [][]string) [][]coord {
 	return coords_slice
 }
 
+func getNumberCoordinatesTwo(data [][]string) [][]coord {
+	var coords_slice [][]coord
+
+	for idx, val := range data { // Gives []string
+		var tmp_co []coord
+		for sidx, sval := range val {
+			_, err := strconv.Atoi(sval) // Trying to cast from string to int
+			if err == nil {              // If the cast succeeded, we know the value is an integer
+				tmp_co = append(tmp_co, coord{x: idx, y: sidx, val: sval})
+			} else {
+				// If the cast failed, and we have values in our "cache"
+				// we flush those to the main 2d array and clear the cache.
+				if len(tmp_co) > 0 {
+					coords_slice = append(coords_slice, tmp_co)
+					tmp_co = []coord{}
+				}
+			}
+			if sidx == len(val)-1 {
+				if len(tmp_co) > 0 {
+					coords_slice = append(coords_slice, tmp_co)
+					tmp_co = []coord{}
+				}
+			}
+		}
+	}
+
+	return coords_slice
+}
+
+func getCoordinatesForGear(data [][]string) []coord {
+	var coords_slice []coord
+	for idx, val := range data { // Gives []string
+		for sidx, sval := range val {
+			if sval == "*" {
+				coords_slice = append(coords_slice, coord{x: idx, y: sidx})
+			}
+		}
+	}
+	return coords_slice
+}
+
+func checkSurrounding(gears_coordinates []coord, parts_coordinates [][]coord) int {
+	var res int
+	for _, gear := range gears_coordinates {
+		// Deep copy the original slice since we'll need it again for the next iteration
+		temp_parts_coordinates := make([][]coord, len(parts_coordinates))
+		copy(temp_parts_coordinates, parts_coordinates)
+
+		var found [][]coord
+		for part_slice_idx, part_slice := range temp_parts_coordinates {
+			if len(temp_parts_coordinates) < part_slice_idx {
+				break
+			}
+			for _, part := range part_slice {
+				// Above
+				if gear.x-1 == part.x {
+					//  Above left          Above mid           Above right
+					if gear.y-1 == part.y || gear.y == part.y || gear.y+1 == part.y {
+						found = append(found, temp_parts_coordinates[part_slice_idx])
+						temp_parts_coordinates[part_slice_idx] = []coord{}
+						break
+					}
+				} else if gear.x+1 == part.x {
+					//  Below left          Below mid           Below right
+					if gear.y-1 == part.y || gear.y == part.y || gear.y+1 == part.y {
+						found = append(found, temp_parts_coordinates[part_slice_idx])
+						temp_parts_coordinates[part_slice_idx] = []coord{}
+						break
+					}
+				} else if gear.x == part.x {
+					// Left or right
+					if gear.y-1 == part.y || gear.y+1 == part.y {
+						found = append(found, temp_parts_coordinates[part_slice_idx])
+						temp_parts_coordinates[part_slice_idx] = []coord{}
+						break
+					}
+				}
+			}
+		}
+
+		if len(found) == 2 {
+			var prod int
+			for _, i := range found {
+				var tmpstr string
+				for _, j := range i {
+					tmpstr += j.val
+				}
+				tmpint, _ := strconv.Atoi(tmpstr)
+				if prod == 0 {
+					prod += tmpint
+				} else {
+					prod *= tmpint
+				}
+			}
+			res += prod
+		}
+	}
+	return res
+}
+
 func one(data []string) {
 	parsed_data := parse(data)
 	coordinates := getNumberCoordinates(parsed_data)
@@ -140,9 +241,18 @@ func one(data []string) {
 	fmt.Printf("Result 1: %v\n", res)
 }
 
+func two(data []string) {
+	parsed_data := parse(data)
+	gears_co := getCoordinatesForGear(parsed_data)
+	coordinates := getNumberCoordinatesTwo(parsed_data)
+	res := checkSurrounding(gears_co, coordinates)
+	fmt.Printf("Result 2: %v\n", res)
+}
+
 func main() {
 	// data := readinput.ReadText("example1.txt")
 	data := readinput.ReadText("input.txt")
 
 	one(data)
+	two(data)
 }
